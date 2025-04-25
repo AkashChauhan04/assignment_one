b6be3ae9846eb559d745b5b870b02a56b704258d
AkashChauhan04 <ac435563@gmail.com>
2025-04-25 12:01:39 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-// import CreateAccount from "./componets/CreateAccount"
-// import Home from "./componets/Home"
-// import Login from "./componets/Login"
-// import UserProfile from "./componets/UserProfile"
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-
-// function App() {
-//   return (
-//     <>
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/create-account" element={<CreateAccount />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/user-profile" element={<UserProfile />} />
-//       </Routes>
-//     </Router>
-
-//     </>
-//   )
-// }
-
-// export default App
-
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./componets/Home";
